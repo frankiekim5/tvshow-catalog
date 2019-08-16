526826b2f83bb590c80a26c020411ce1364427a6
suebyeon <suebyeon25@gmail.com>
2019-08-16 01:55:58 -0400

    adding venv

--- backend/test.py
+++ backend/test.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 # Config MySQL
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Frankie1999!'
-app.config['MYSQL_DB'] = 'atlbeltline'
+app.config['MYSQL_PASSWORD'] = 'FrankieandSue'
+app.config['MYSQL_DB'] = 'tvshow-catalog'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 # Init MySQL
 mysql = MySQL(app)
